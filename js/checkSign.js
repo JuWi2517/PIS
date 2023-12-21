f49bc6549286dd1af2df9d829802d36bdd2ce34d
Honzaa <115077127+JuWi2517@users.noreply.github.com>
2023-12-21 11:10:31 +0100

    Update checkSign.js

--- js/checkSign.js
+++ js/checkSign.js
@@ -1,12 +1,12 @@
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // User is signed in
+      
       console.log('User is signed in:', user);
-      // You can also redirect to a different page if necessary
+     
     } else {
-      // No user is signed in
+      
       console.log('No user is signed in');
-      // Redirect to the login page or handle accordingly
+      
       window.location.href = 'login.html';
     }
 });
